a17e3b81ca7f4d6911c05cacfea635429760f3cf
Lucas Romero <lucasromero674@gmail.com>
2020-07-21 11:54:24 -0300

    comp drawer

---
+++ src/components/productos/CompDrawer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Drawer} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        width: 300,
+        backgroundColor:"#7fdbda",
+        color:"pink",
+        opacity: '99%'
+    }
+}));
+
+const CompDrawer = (props) => {
+
+    const classes = useStyles()
+
+    return(
+        <Drawer
+        variant="permanent"
+        classes={{
+            root: classes.root,
+            paper: classes.root,
+        }}
+        anchor="left"
+        >
+            
+        </Drawer>
+    )
+}
+
+export default CompDrawer;
